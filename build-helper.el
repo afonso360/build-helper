dab95307dcf8321c052c626a6fb86eeb6514c833
Afonso Bordado <afonsobordado@az8.co>
2016-10-03 18:17:33 +0100

    Update Goals and finish renaming lists to targets

--- build-helper.el
+++ build-helper.el
@@ -22,40 +22,47 @@
 
 ;;; Commentary:
 ;; Goals:
-;; Provide multiple compile-history lists (build, run, test, etc..)
-;; Provide a recompile and compile function for each list
-;; Per project lists
+;; Provide compile-history target per target (build, run, test, etc..)
+;; Provide a recompile and compile function for each target
+;; Provide a function to clear projects
+;; Provide a command number limit
+;; Should be able to set comint option per target
+;; Per project targets
 ;;   build-helper will switch to the project root dir before running any of the commands
 ;;   Require projectile and use that
-;;   Have a .build-helper-lists.el that maintains the list of these projects and their lists
-;;     .build-helper-list.el should be roughly like the following
-;;     '(("project/root/dir" . ((c-mode . ((build . '("make" "gcc foo.c"))
-;;     	                                   (run   . '("./a.out"))
-;;     	                                   (test  . '("make run"))))
-;;     			        (java-mode . ((build . '("javac main.java"))))))
+;;   Per target comint setting defaults to nil
+;;   Have a .build-helper-targets.el that maintains the target of these projects and their targets
+;;     .build-helper-targets.el should be roughly like the following
+;;     '(("project/root/dir" . ((c-mode . ((build . ("make" "gcc foo.c"))
+;;     	                                   (run   . ("./a.out"))
+;;     	                                   (test  . ("make run"))))
+;;     			        (java-mode . ((build . ("javac main.java"))))))
 ;;
-;;       ("project2/root/dir" . ((elisp-mode . ((test . '("elisp test command"))))))))
+;;       ("project2/root/dir" . ((elisp-mode . ((test . ("elisp test command"))))))))
 ;;
 ;;
-;;     From the example above, java-mode would never have run or test commands
-;;     because those would be elisp functions.  (this is just an example)
+;;     From the example above, java-mode would never have run or test targets
+;;     because those would be elisp functions that would never fail.  (this is just an example)
 ;;
 ;; Optionally allow compilation functions
 ;;   Compilation functions are based on the major-mode from which they are invoked
 ;;   The return value should be checked to allow multiple chains of compilation functions
-;;   Check for a modified compile-history (if that exists use that on the recompile, otherwise run it again)
+;;   Maybe: Check for a modified compile-history (if that exists use that on the recompile, otherwise run it again)
 
 
 ;; Some code based on company-statistics.el (https://github.com/company-mode/company-statistics)
 ;;; Usage:
 
 ;;; Code:
+(require 'projectile)
+(require 'gv)
+
 (defgroup build-helper nil
   "Helper functions to build files."
   :group 'build-helper)
 
 (defcustom build-helper-file
-  (concat user-emacs-directory ".build-helper-lists.el")
+  (concat user-emacs-directory ".build-helper-targets.el")
   "File to save build-helper command history."
   :type 'string)
 
