28b56947f075ae75722fb934757dd818732065c2
Afonso Bordado <afonsobordado@az8.co>
2016-10-07 19:06:09 +0100

    Update documentation

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
-# Build Helper [![Build Status](https://travis-ci.org/afonso360/build-helper.svg?branch=master)](https://travis-ci.org/afonso360/build-helper)
+# Build Helper
+[![Build Status](https://travis-ci.org/afonso360/build-helper.svg?branch=master)](https://travis-ci.org/afonso360/build-helper)
 
-Build helper is a set of functions to simplify building code.
+Build helper helps you set your build configurations in order.
+
+It enables you to have different commands to build, run, test, etc.. per project.
+
+Commands persist across sessions and in the same project different major modes can have different run commands
+
+
+## Usage:
+
+For a quick setup add these lines to your init.el
+
+``` lisp
+(require 'build-helper)
+(build-helper-setup)
+```
+and M-x build-helper-run to build
+you can then use M-x build-helper-re-run to run the last command
+
+You can bind these to your preferred keys, however there are convinience
+functions that run and re-run common targets (run, build, test)
+these have the name build-helper-run-TARGET and build-helper-re-run-TARGET
+replacing target with one of the 3 preset targets
+
+## Advanced functionality:
+
+build-helper can also run functions from other packages, for example
+if you use cmake-ide-mode, you will most certainly want to execute
+`cmake-ide-compile' when executing the run target on c++-mode
+to achieve this add the following code to the init.el file
+
+``` lisp
+(build-helper-add-function 'c++-mode 'run #'cmake-ide-compile)
+```
+
+This adds the function `cmake-ide-compile' to be executed when the user
+executes the "run" target in c++-mode on any project
+
+### Function return values:
+
+The return value of the executed functions is checked, when t no other function
+will be executed and the user will not be prompted for a command.
+If all the functions return nil the user will be prompted as usual
+
+To ensure a function returns t you can add the following instead
+
+``` lisp
+(build-helper-add-function 'c++-mode 'run #'(lambda () (cmake-ide-compile) t))
+```
--- build-helper.el
+++ build-helper.el
@@ -21,19 +21,53 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Goals:
-;; Optionally allow compilation functions
-;;   Compilation functions are based on the major-mode from which they are invoked
-;;   The return value should be checked to allow multiple
-;;chains of compilation functions
+;; build-helper helps you set your build configurations in order.
+;; enabling you to have different commands to build, run, test, etc.. per project
+;; these commands persist across Emacs sessions and in the same project
+;; different major modes can have different run commands
+
 ;; Todo:
 ;; build-helper--targets gets a duplicate entry every time we add a command
 ;; can't run commands with spaces
+;; execute functions
+
+;;; Usage:
+;; For a quick setup add these lines to your init.el
+;;
+;; (require 'build-helper)
+;; (build-helper-setup)
+;;
+;; and M-x build-helper-run to build
+;; you can then use M-x build-helper-re-run to run the last command
+;;
+;; you can bind these to your preferred keys, however there are convinience
+;; functions that run and re-run common targets (run, build, test)
+;; these have the name build-helper-run-TARGET and build-helper-re-run-TARGET
+;; replacing target with one of the 3 preset targets
+;;
+;; Advanced functionality
+;; build-helper can also run functions from other packages, for example
+;; if you use cmake-ide-mode, you will most certainly want to execute
+;; `cmake-ide-compile' when executing the run target on c++-mode
+;; to achieve this add the following code to the init.el file
+;;
+;; (build-helper-add-function 'c++-mode 'run #'cmake-ide-compile)
+;;
+;; This adds the function `cmake-ide-compile' to be executed when the user
+;; executes the "run" target in c++-mode on any project
+;;
+;; Function return values:
+;; The return value of the executed functions is checked, when t no other function
+;; will be executed and the user will not be prompted for a command.
+;; If all the functions return nil the user will be prompted as usual
+;;
+;; To ensure a function returns t you can add the following instead
+;;
+;; (build-helper-add-function 'c++-mode 'run #'(lambda () (cmake-ide-compile) t))
+
 
 ;; Some code based on company-statistics.el
 ;; https://github.com/company-mode/company-statistics
-
-;;; Usage:
 
 ;;; Code:
 (require 'projectile)
